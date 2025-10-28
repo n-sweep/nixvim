f45f0532bda6c88aa43ddafbb1d061a904440644
n-sweep <34486798+n-sweep@users.noreply.github.com>
2025-10-28 07:25:15 -0400

    r stuff

--- config/plugins/default.nix
+++ config/plugins/default.nix
@@ -81,6 +81,7 @@ in
     "plugin/colorschemes.lua".source = ./lua/colorschemes.lua;
     "plugin/foundry.lua".source = ./lua/foundry.lua;
     "plugin/misc.lua".source = ./lua/misc.lua;
+    "plugin/r-nvim.lua".source = ./lua/r-nvim.lua;
     "plugin/tmux.lua".source = ./lua/tmux.lua;
     "plugin/vim-dadbod.lua".source = ./lua/vim-dadbod.lua;
   };
