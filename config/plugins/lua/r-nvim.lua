487ff47b96c3d5b0b309722a66edc128eff6569b
n-sweep <34486798+n-sweep@users.noreply.github.com>
2025-10-28 07:36:09 -0400

    r stuff

--- config/plugins/lua/r-nvim.lua
+++ config/plugins/lua/r-nvim.lua
@@ -1,8 +1,5 @@
-require("r.nvim").setup({
-    config = function()
-        local opts = {
-            external_term = "tmux split-window -h -l 80"
-        }
-        require("r").setup(opts)
-    end
-})
+local opts = {
+    external_term = "tmux split-window -h -l 80"
+}
+
+require("r").setup(opts)
