e8013b907146e2a86fccf7398e8bf35315f0a084
n-sweep <34486798+n-sweep@users.noreply.github.com>
2025-10-31 09:46:14 -0400

    rugh

--- config/plugins/default.nix
+++ config/plugins/default.nix
@@ -76,13 +76,12 @@ in
 
   ];
 
-  extraConfigLua = builtins.readFile ./lua/r-nvim.lua;
   extraFiles = {
     # nvim plugins
     "plugin/colorschemes.lua".source = ./lua/colorschemes.lua;
     "plugin/foundry.lua".source = ./lua/foundry.lua;
     "plugin/misc.lua".source = ./lua/misc.lua;
-    # "plugin/r-nvim.lua".source = ./lua/r-nvim.lua;
+    "plugin/r-nvim.lua".source = ./lua/r-nvim.lua;
     "plugin/tmux.lua".source = ./lua/tmux.lua;
     "plugin/vim-dadbod.lua".source = ./lua/vim-dadbod.lua;
   };
