027e00f1cbd4eae8d2db4d85614888ae8076f8eb
n-sweep <34486798+n-sweep@users.noreply.github.com>
2025-12-28 15:45:25 -0500

    treesitter updates

--- config/plugins/treesitter.nix
+++ config/plugins/treesitter.nix
@@ -1,38 +1,39 @@
-{ ... }: {
+{ pkgs, ... }: {
 
   plugins.treesitter = {
     enable = true;
+
+    grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
+      bash
+      c
+      diff
+      gitattributes
+      gitcommit
+      git_config
+      gitignore
+      git_rebase
+      go
+      json
+      lua
+      markdown
+      markdown_inline
+      nix
+      python
+      r
+      rnoweb
+      regex
+      sql
+      ssh_config
+      tmux
+      vhs
+      vim
+      vimdoc
+      yaml
+    ];
+
     settings = {
       highlight.enable = true;
       parser_install_dir = "~/.cache/nvim/parsers";
-      ensure_installed = [
-        # https://github.com/nvim-treesitter/nvim-treesitter?tab=readme-ov-file#supported-languages
-        "bash"
-        "c"
-        "diff"
-        "gitattributes"
-        "gitcommit"
-        "git_config"
-        "gitignore"
-        "git_rebase"
-        "go"
-        "json"
-        "lua"
-        "markdown"
-        "markdown_inline"
-        "nix"
-        "python"
-        "r"
-        "rnoweb"
-        "regex"
-        "sql"
-        "ssh_config"
-        "tmux"
-        "vhs"
-        "vim"
-        "vimdoc"
-        "yaml"
-      ];
     };
   };
 
