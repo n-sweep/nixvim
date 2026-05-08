00245ff87a7125ecda074eeb40e6d2b1f1d8f996
n-sweep <34486798+n-sweep@users.noreply.github.com>
2026-05-08 16:03:37 -0400

    follow nixpkgs unstable

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixvim.url = {
+    nixvim = {
       url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
     };
