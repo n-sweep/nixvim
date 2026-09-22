fc52284a27904c15576353d6eea271be4dbfa27b
n-sweep <34486798+n-sweep@users.noreply.github.com>
2026-09-22 11:40:08 -0400

    colorschemes

--- modules/plugins/_lua/colorschemes.lua
+++ modules/plugins/_lua/colorschemes.lua
@@ -4,7 +4,9 @@ local schemes = {
   oryxpro = "kanagawa-dragon",
   osgiliath = "kanagawa",
   robot_house = "gruvbox",
-  LWV4JGTWHV = "base16-solarflare"
+  LWV4JGTWHV = "base16-solarflare",
+  FG4W0FN64X = "base16-solarflare"
+
 }
 
 local scheme = schemes[hostname] or "tokyonight"
